97fc67a0dce9fdf7ad97017abdc4af3186dfc058
hieu15402 <hieunm15402@gmail.com>
2025-01-19 14:12:14 +0700

    update status error

--- src/main/java/com/example/managebook/exception/ErrorCode.java
+++ src/main/java/com/example/managebook/exception/ErrorCode.java
@@ -10,10 +10,10 @@ public enum ErrorCode {
     INVALID_TITLE(400, "Title can not be null or empty", HttpStatus.BAD_REQUEST),
     INVALID_DATE(400, "Date can not be null or empty", HttpStatus.BAD_REQUEST),
     INVALID_ISBN(400, "INVALID_ISBN can not be null or empty", HttpStatus.BAD_REQUEST),
-    DUPLICATE_ISBN(400, "ISBN must be unique", HttpStatus.NOT_FOUND),
+    DUPLICATE_ISBN(409, "ISBN must be unique", HttpStatus.CONFLICT),
     INVALID_AUTHOR_ID(404, "Author may be null or do not exist", HttpStatus.NOT_FOUND),
     INVALID_BOOK_ID(404, "Book may be null or do not exist", HttpStatus.NOT_FOUND),
-    FAIL_TO_DELETE(404, "Failed to delete the book", HttpStatus.BAD_REQUEST),
+    FAIL_TO_DELETE(400, "Failed to delete the book", HttpStatus.BAD_REQUEST),
     UNCATEGORIZED_EXCEPTION(500, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final int code;
